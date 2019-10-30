ba6f0daeeea978d6dff5ebbd22363f879810801f
pow_580 <wollowjk@gmail.com>
2019-10-30 14:25:48 +0700

    My nineth commit files

--- src/main/java/com/pawin/linebotspringboot/LineBotController.java
+++ src/main/java/com/pawin/linebotspringboot/LineBotController.java
@@ -5,12 +5,16 @@
  */
 package com.pawin.linebotspringboot;
 
+import com.google.common.io.ByteStreams;
+import com.linecorp.bot.client.MessageContentResponse;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.LocationMessageContent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.ImageMessage;
 import com.linecorp.bot.model.message.LocationMessage;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.StickerMessage;
@@ -19,13 +23,25 @@ import com.linecorp.bot.model.profile.UserProfileResponse;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+
 import lombok.NonNull;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -125,4 +141,78 @@ public class LineBotController {
                 , message.getLongitude()
         ));
     }
+    
+    //send Image
+    @EventMapping
+    public void handleImageMessage(MessageEvent<ImageMessageContent> event) {
+        log.info(event.toString());
+        ImageMessageContent content = event.getMessage();
+        String replyToken = event.getReplyToken();
+
+        try {
+            MessageContentResponse response = lineMessagingClient.getMessageContent(
+                content.getId()).get();
+            DownloadedContent jpg = saveContent("jpg", response);
+            DownloadedContent previewImage = createTempFile("jpg");
+
+            system("convert", "-resize", "240x",
+                    jpg.path.toString(),
+                    previewImage.path.toString());
+
+            reply(replyToken, new ImageMessage(jpg.getUri(), previewImage.getUri()));
+
+        } catch (InterruptedException | ExecutionException e) {
+            reply(replyToken, new TextMessage("Cannot get image: " + content));
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void system(String... args) {
+        ProcessBuilder processBuilder = new ProcessBuilder(args);
+        try {
+            Process start = processBuilder.start();
+            int i = start.waitFor();
+            log.info("result: {} => {}", Arrays.toString(args), i);
+        } catch (InterruptedException e) {
+            log.info("Interrupted", e);
+            Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static DownloadedContent saveContent(String ext, 
+                                                 MessageContentResponse response) {
+        log.info("Content-type: {}", response);
+        DownloadedContent tempFile = createTempFile(ext);
+        try (OutputStream outputStream = Files.newOutputStream(tempFile.path)) {
+            ByteStreams.copy(response.getStream(), outputStream);
+            log.info("Save {}: {}", ext, tempFile);
+            return tempFile;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static DownloadedContent createTempFile(String ext) {
+        String fileName = LocalDateTime.now() + "-" 
+                          + UUID.randomUUID().toString() 
+                          + "." + ext;
+        Path tempFile = LineBotSpringBootApplication.downloadedContentDir.resolve(fileName);
+        tempFile.toFile().deleteOnExit();
+        return new DownloadedContent(tempFile, createUri("/downloaded/" + tempFile.getFileName()));
+
+    }
+
+    private static String createUri(String path) {
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(path).toUriString();
+    }
+
+    @Value
+    public static class DownloadedContent {
+        Path path;
+        String uri;
+    }    
 }
--- src/main/java/com/pawin/linebotspringboot/LineBotSpringBootApplication.java
+++ src/main/java/com/pawin/linebotspringboot/LineBotSpringBootApplication.java
@@ -1,11 +1,15 @@
 package com.pawin.linebotspringboot;
 
+import com.fasterxml.jackson.databind.ser.std.FileSerializer;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,7 +17,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@LineMessageHandler
 public class LineBotSpringBootApplication {
 
-	public static void main(String[] args) {
+        static Path downloadedContentDir;
+        
+	public static void main(String[] args) throws IOException {
+                downloadedContentDir = Files.createTempDirectory("line-bot");
 		SpringApplication.run(LineBotSpringBootApplication.class, args);
 	}
 
